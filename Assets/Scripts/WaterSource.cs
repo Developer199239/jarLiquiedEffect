5d622b9309c81c9787629463d418eccd5a87834e
Developer199239 <developer199239@gmail.com>
2018-12-03 20:53:25 +0600

    first commit

---
+++ Assets/Scripts/WaterSource.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterSource : MonoBehaviour {
+    public GameObject waterPrefabs;
+    private bool isSpawnSession = false;
+    public bool isSpawnRunning = true;
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(isSpawnSession == false && isSpawnRunning)
+        StartCoroutine(SpawnRoutine());
+    }
+
+
+    IEnumerator SpawnRoutine()
+    {
+        isSpawnSession = true;
+        Instantiate(waterPrefabs, new Vector3(0, 7, 0), Quaternion.identity);
+            yield return new WaitForSeconds(0.2f);
+        isSpawnSession = false;
+    }
+}
